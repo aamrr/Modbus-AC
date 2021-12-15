510f0e741e6ba4cc887f151046f492faf027de8a
Mohamed amer weslati <56867182+aamrr@users.noreply.github.com>
2021-12-15 09:18:54 +0100

    first - working without UI

---
+++ capteur.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+"""
+Pymodbus Synchronous Client Examples
+--------------------------------------------------------------------------
+
+The following is an example of how to use the synchronous modbus client
+implementation from pymodbus.
+
+It should be noted that the client can also be used with
+the guard construct that is available in python 2.5 and up::
+
+    with ModbusClient('127.0.0.1') as client:
+        result = client.read_coils(1,10)
+        print result
+"""
+# --------------------------------------------------------------------------- #
+# import the various client implementations
+# --------------------------------------------------------------------------- #
+from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+# from pymodbus.client.sync import ModbusUdpClient as ModbusClient
+# from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+
+# --------------------------------------------------------------------------- #
+# configure the client logging
+# --------------------------------------------------------------------------- #
+import logging
+FORMAT = ('%(asctime)-15s %(threadName)-15s '
+          '%(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+UNIT = 0x1
+
+
+def run_sync_client():
+    
+    from pymodbus.transaction import ModbusRtuFramer
+
+    client = ModbusClient('127.0.0.1', retries=3, port=5020)
+    
+    client.connect()
+
+    
+
+
+    temp1 = 31
+    temp2 = 22  
+    temp3 = 23
+
+    log.debug("Write to multiple holding registers and read back")
+    rq = client.write_registers(1, [temp1, temp2, temp3], unit=UNIT)
+    rr = client.read_holding_registers(1, 3, unit=UNIT)
+    assert(not rq.isError())     # test that we are not an error
+    assert(not rr.isError())     # test that we are not an error
+    assert(rr.registers == [temp1, temp2, temp3])      # test the expected value
+
+    
+    # ----------------------------------------------------------------------- #
+    # close the client
+    # ----------------------------------------------------------------------- #
+    client.close()
+
+
+if __name__ == "__main__":
+    run_sync_client()
---
+++ server.py
@@ -0,0 +1,53 @@
+from pymodbus.version import version
+from pymodbus.server.sync import StartTcpServer
+from pymodbus.server.sync import StartTlsServer
+from pymodbus.server.sync import StartUdpServer
+from pymodbus.server.sync import StartSerialServer
+
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.datastore import ModbusSequentialDataBlock, ModbusSparseDataBlock
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+
+import socket
+
+from pymodbus.transaction import ModbusRtuFramer, ModbusBinaryFramer
+import logging
+
+FORMAT = ('%(asctime)-15s %(threadName)-15s'
+          ' %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+
+def run_server():
+    store = ModbusSlaveContext(
+        di=ModbusSequentialDataBlock(0, [17]*100),
+        co=ModbusSequentialDataBlock(0, [17]*100),
+        hr=ModbusSequentialDataBlock(0, [17]*100),
+        ir=ModbusSequentialDataBlock(0, [17]*100))
+    
+    context = ModbusServerContext(slaves=store, single=True)
+
+    # ----------------------------------------------------------------------- #
+    # initialize the server information
+    # ----------------------------------------------------------------------- #
+    # If you don't set this or any fields, they are defaulted to empty strings.
+    # ----------------------------------------------------------------------- #
+    identity = ModbusDeviceIdentification()
+    identity.VendorName = 'Pymodbus'
+    identity.ProductCode = 'PM'
+    identity.VendorUrl = 'http://github.com/riptideio/pymodbus/'
+    identity.ProductName = 'Pymodbus Server'
+    identity.ModelName = 'Pymodbus Server'
+    identity.MajorMinorRevision = version.short()
+    
+
+    # ----------------------------------------------------------------------- #
+    # run the server you want
+    # ----------------------------------------------------------------------- #
+    # Tcp:
+    StartTcpServer(context, identity=identity, address=("127.0.0.1", 5020))
+
+if __name__ == "__main__":
+    run_server()
